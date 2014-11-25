ce3563e4d413d3f1109c33b34cbb5b695b32cab5
Joshua Ohlman <codewriterohs@gmail.com>
2014-11-25 15:33:18 -0600

    Publish version v0.1.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'cwohlman:geocoded-collection',
   summary: 'Auto-geocode your collection documents',
-  version: "0.1.1",
+  version: "0.1.2",
   git: 'https://github.com/cwohlman/meteor-geocoded-collections.git'
 });
 
